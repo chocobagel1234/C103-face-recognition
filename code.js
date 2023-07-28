d4196e55c0a437e371580333591fb8fd6f751b31
chocobagel1234 <118059248+chocobagel1234@users.noreply.github.com>
2023-07-28 16:05:16 +0100

    Add files via upload

--- code.js
+++ code.js
@@ -16,8 +16,24 @@ function take_snapshot(){
 }
  console.log("ml5 version:", ml5.version);
 
- classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/qx8eVoEJR/model.json", modelLoaded);
+ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/qx8eVoEJR/model.json", modelLoaded, modelReady);
 
 function modelLoaded() {
     console.log("Model Loaded!");
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error)
+    }
+    else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+        document.getElementById("object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
